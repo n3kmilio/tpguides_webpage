51cc69371b1943e8f5ae11c3ba85a05c0ed8585b
379663 <mathis.sander@stud.th-luebeck.de>
2025-01-10 00:17:18 +0100

    zz

--- src/main/java/tpguides/controller/WriteController.java
+++ src/main/java/tpguides/controller/WriteController.java
@@ -2,7 +2,6 @@ package tpguides.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import tpguides.model.Guide;
@@ -13,6 +12,9 @@ public class WriteController {
     @Autowired
     private GuideRepository guideservice;
 
+    public WriteController(GuideRepository guideRepository) {
+        this.guideservice = guideRepository;
+    }
 
     @PostMapping("/submit")
     public String writeSubmit (@RequestParam("guide-input") String Content,
