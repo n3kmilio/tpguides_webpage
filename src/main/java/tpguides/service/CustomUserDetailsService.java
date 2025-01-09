c9e1e9f59ed56d410a2bed1e3b639eaf05c6a26a
Emilio <emilio.gaebler@stud.th-luebeck.de>
2025-01-09 16:53:34 +0100

    myProfile_dynamicUsername

--- src/main/java/tpguides/service/CustomUserDetailsService.java
+++ src/main/java/tpguides/service/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package tpguides.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
