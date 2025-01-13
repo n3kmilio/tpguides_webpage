e04ae50772ccd29c53822e6750bc832e9fe150a4
Emilio <emilio.gaebler@stud.th-luebeck.de>
2025-01-13 01:17:33 +0100

    Search_for_Profiles

--- src/main/java/tpguides/controller/UserController.java
+++ src/main/java/tpguides/controller/UserController.java
@@ -3,12 +3,10 @@ package tpguides.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tpguides.model.User;
 import tpguides.repository.UserRepository;
+import tpguides.service.UserService;
 
 import java.util.Optional;
 
@@ -18,6 +16,7 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    private UserService userService;
 
     // Suche Benutzer basierend auf dem Benutzernamen
     @GetMapping("/search")
@@ -27,5 +26,10 @@ public class UserController {
             return ResponseEntity.ok(user.get());
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Benutzer nicht gefunden");
+    }
+
+    public Optional<User> getUserByUsername(@PathVariable String username) {
+        System.out.println("API-Aufruf für Benutzer: " + username);
+        return userService.findByUsername(username);
     }
 }
