9fc5850d7b5698a2bcc9a5b12c91ed4b7372f438
Erik Buchholz <erik.buchholz@stud.th-luebeck.de>
2025-01-15 00:38:44 +0100

    editUser W.I.P

---
+++ src/main/resources/static/Javascript/editUser.js
@@ -0,0 +1,20 @@
+const editButton = document.getElementById("editButton");
+const saveButton = document.getElementById("saveButton");
+const paragraph = document.getElementById("editableText");
+const textarea = document.getElementById("textEditor");
+
+editButton.addEventListener("click", () => {
+   textarea.value = paragraph.textContent;
+   paragraph.classList.add("hidden");
+   textarea.classList.remove("hidden");
+   saveButton.classList.remove("hidden");
+   editButton.classList.add("hidden");
+});
+
+saveButton.addEventListener("click", () => {
+    paragraph.textContent = textarea.value;
+    paragraph.classList.remove("hidden");
+    textarea.classList.add("hidden");
+    saveButton.classList.add("hidden");
+    editButton.classList.remove("hidden");
+});
