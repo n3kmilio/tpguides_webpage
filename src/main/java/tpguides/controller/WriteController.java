5784cd6c9429876a3b2d5651274499f68a850af5
379663 <mathis.sander@stud.th-luebeck.de>
2025-01-15 17:47:41 +0100

    trail and error

--- src/main/java/tpguides/controller/WriteController.java
+++ src/main/java/tpguides/controller/WriteController.java
@@ -1,34 +1,37 @@
 package tpguides.controller;
 
-import jakarta.persistence.criteria.Selection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import tpguides.model.Guide;
 import tpguides.repository.GuideRepository;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
-@Controller
-@RequestMapping(method={RequestMethod.POST},value = "/dropdownButto")
+@RestController
+@RequestMapping("/api/write")
 public class WriteController {
+
     @Autowired
     private GuideRepository guideservice;
+    private Guide guide;
 
     public WriteController(GuideRepository guideRepository) {
+        this.guide = new Guide();
         this.guideservice = guideRepository;
     }
 
+    @PostMapping(value = "/search", consumes = "application/json")
+    public String getGame(@RequestBody String json) {
+        guide.setGame(json);
+        return "redirect:/";
+    }
+
     @PostMapping("/write")
-    public @ResponseBody String writeSubmit (@RequestParam("guide-input") String Content,
+    public String writeSubmit (@RequestParam("guide-input") String Content,
                                @RequestParam("title-input") String Title,
-                               @RequestParam("desc-input")  String Desc,
-                               @RequestBody String Game) {
-        Guide guide = new Guide();
+                               @RequestParam("desc-input")  String Desc) {
         guide.setContent(Content);
         guide.setDescription(Desc);
         guide.setTitle(Title);
-        guide.setGame(String.valueOf(Game));
         guideservice.save(guide);
         return "redirect:/";
     }
